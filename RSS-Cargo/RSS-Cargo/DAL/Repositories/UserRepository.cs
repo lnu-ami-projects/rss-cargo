2bc206746b7ebdb13a03f56ef86c9cbf2134ff47
Likor7 <sov30082003@gmail.com>
2022-11-06 14:10:07 +0200

    Finished repositories

--- RSS-Cargo/RSS-Cargo/DAL/Repositories/UserRepository.cs
+++ RSS-Cargo/RSS-Cargo/DAL/Repositories/UserRepository.cs
@@ -49,4 +49,64 @@ public class UserRepository
         _context.Users.Remove(user);
         _context.SaveChanges();
     }
+
+    public void AddCargoUser(int userId, int cargoId)
+    {
+        var user = _context.Users.FirstOrDefault(x => x.Id == userId);
+        if (user == null)
+        {
+            throw new ArgumentException("User Id not found");
+        }
+
+        user.UserCargos.Add(new UserCargo{UserId = userId, CargoId = cargoId});
+        _context.SaveChanges();
+    }
+    
+    public void DeleteCargoUser(int userId, int cargoId)
+    {
+        var user = _context.Users.FirstOrDefault(x => x.Id == userId);
+        if (user == null)
+        {
+            throw new ArgumentException("User Id not found");
+        }
+        
+        user.UserCargos.Remove(new UserCargo{UserId = userId, CargoId = cargoId});
+        _context.SaveChanges();
+    }
+    
+    public void AddFeedUser(int userId, string feed)
+    {
+        if (!Uri.IsWellFormedUriString(feed, UriKind.Absolute))
+        {
+            throw new ArgumentException("This is not an URL " + feed);
+        }
+        
+        var user = _context.Users.FirstOrDefault(x=> x.Id == userId);
+        
+        if (user == null)
+        {
+            throw new ArgumentException("There is no id like this " + userId);
+        }
+        
+        user.UserFeeds.Add(new UserFeed{RssFeed = feed});
+        _context.SaveChanges();
+    }
+    
+    public void DeleteFeedUser(int userId, string feed)
+    {
+        if (!Uri.IsWellFormedUriString(feed, UriKind.Absolute))
+        {
+            throw new ArgumentException("This is not an URL " + feed);
+        }
+        
+        var userFeed = _context.UserFeeds.FirstOrDefault(x=> x.UserId == userId && x.RssFeed == feed);
+        
+        if (userFeed == null)
+        {
+            throw new ArgumentException("There is no feed like this in this cargo");
+        }
+        
+        _context.UserFeeds.Remove(userFeed);
+        _context.SaveChanges();
+    }
 }
